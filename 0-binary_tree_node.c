0fc80477be9351ff25c26a158d594fc3b9850fa2
FrancoDoldan0 <francodoldan23@gmail.com>
2024-01-03 07:13:03 -0800

    add task 0

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_node - create a new node without childs
+* @parent: refears to the parent of the node
+* @value: value in the node
+*/
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new = malloc(sizeof(binary_tree_t));
+
+	if (!new)
+		return (0);
+
+	new->n = value;
+	new->parent = parent;
+	new->right = NULL;
+	new->left = NULL;
+
+	return (new);
+}
+
