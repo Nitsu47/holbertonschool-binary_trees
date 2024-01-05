cbd722ff9f97114d15f909d7054e815e101349aa
FrancoDoldan0 <francodoldan23@gmail.com>
2024-01-05 11:26:08 -0800

    add task 5

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -8,9 +8,9 @@
 
 int binary_tree_is_root(const binary_tree_t *node)
 {
-	if (node == NULL) /*si el nodo es nulo es raiz*/
+	if (node == NULL)
 	{
-		return (1);
+		return (0);
 	}
 
 	if (node->parent != NULL) /*si el nodo padre tiene hijos no es raiz*/
