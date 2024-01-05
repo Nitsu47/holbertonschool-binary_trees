5161b0a75d181edcf9248e3b13499bf52ae558f4
FrancoDoldan0 <francodoldan23@gmail.com>
2024-01-04 17:03:26 -0800

    add task 1

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new = malloc(sizeof(binary_tree_t));
+
+	if (!parent)
+		return (0); /* retorna 0 si el parent esta vacio*/
+
+	new = binary_tree_node(parent, value); /*llama a la funcion creada en el ej 0*/
+
+	/*se inserta el nuevo nodo a la izquierda de parent*/
+	if (parent->left)
+    {
+        new->left = parent->left;
+        parent->left->parent = new;
+    }
+
+	parent->left = new; /*el nuevo nodo va a ser el hijo izquierdo*/
+
+	return (new);
+}
