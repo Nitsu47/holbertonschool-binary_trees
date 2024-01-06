8b33714f2b43f703af36b61e4aac7fdf7689db3e
Nitsu47 <agustraversa2004@gmail.com>
2024-01-06 01:22:38 +0000

    task 7 in order traversal

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+/**
+ * 
+ * 
+ * 
+*/
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+    if (!tree)
+    {
+        return;
+    }
+    if (!func)
+    {
+        return;
+    }
+
+    binary_tree_inorder(tree->left, func);
+    func(tree->n);
+    binary_tree_inorder(tree->right, func);
+}
