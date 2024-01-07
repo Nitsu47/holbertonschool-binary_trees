74bcbadf9c6b4431b1617a7fb570b2751fc300d5
FrancoDoldan0 <francodoldan23@gmail.com>
2024-01-07 08:50:40 -0800

    add task 7 whit betty style

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -2,20 +2,21 @@
 /**
  * binary_tree_inorder - travels a binary tree in order traversal
  * @tree: pointer to the root node
- * @func: a pointer to a function to call for each node. The value in the node must be passed as a parameter to this function.
+ * @func: a pointer to a function to call for each node.
+ * The value in the node must be passed as a parameter to this function.
 */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (!tree)
-    {
-        return;
-    }
-    if (!func)
-    {
-        return;
-    }
+	if (!tree)
+	{
+		return;
+	}
+	if (!func)
+	{
+		return;
+	}
 
-    binary_tree_inorder(tree->left, func);
-    func(tree->n);
-    binary_tree_inorder(tree->right, func);
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
 }
