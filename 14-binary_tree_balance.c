46baec0baddb690d304a8d9632ff2b0f7adfb852
Nitsu47 <agustraversa2004@gmail.com>
2024-01-09 21:06:44 +0000

    height function fixed for null childs

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -10,8 +10,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	int left_height = (int)binary_tree_height(tree->left);
-	int right_height = (int)binary_tree_height(tree->right);
+	int left_height = binary_tree_height(tree->left);
+	int right_height = binary_tree_height(tree->right);
 
 	return (left_height - right_height);
 }
@@ -30,16 +30,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		return (0);
 	}
-
-	if (tree->left != NULL)
-	{
 		height_left = 1 + binary_tree_height(tree->left);
-	}
-
-	if (tree->right != NULL)
-	{
 		height_right = 1 + binary_tree_height(tree->right);
-	}
 
 	if (height_left >= height_right)
 	{
@@ -48,4 +40,3 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	return (height_right);
 }
-
