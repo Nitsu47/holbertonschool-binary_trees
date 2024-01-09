5df80e53037022398a3939dce0354c474d02fc00
FrancoDoldan0 <francodoldan23@gmail.com>
2024-01-09 08:52:03 -0800

    add betty style in the task 15,17,18

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -8,7 +8,7 @@
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-    if (!node || !node->parent)
+	if (!node || !node->parent)
 		return (NULL);
 	if (node == node->parent->right)
 		return (node->parent->left);
