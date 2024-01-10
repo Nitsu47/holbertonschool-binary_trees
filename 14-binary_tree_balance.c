21be83dcccb5388ee789025c910bce2435b80a22
Nitsu47 <agustraversa2004@gmail.com>
2024-01-10 00:06:10 +0000

    variables changes

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -6,10 +6,14 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	int left, right;
+
 	if (tree == NULL)
 		return (0);
-	int left = binary_tree_height(tree->left);
-	int right = binary_tree_height(tree->right);
+	left = 0;
+	right = 0;
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
 
 	return (left - right);
 }
@@ -20,12 +24,12 @@ int binary_tree_balance(const binary_tree_t *tree)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t left = 0, right = 0;
+
 	if (tree == NULL)
 		return (0);
-
-	size_t left = binary_tree_height(tree->left) + 1;
-	size_t right = binary_tree_height(tree->right) + 1;
-
+	left = binary_tree_height(tree->left) + 1;
+	right = binary_tree_height(tree->right) + 1;
 	if (left > right)
 		return (left);
 	else
